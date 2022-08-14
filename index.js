6dfcb502d512b63afa5a91ff430cbd8b303acce4
Daniel Teel <daniel.teel.2@us.af.mil>
2022-08-14 03:14:44 -0600

    asdasd

--- index.js
+++ index.js
@@ -1,91 +1,135 @@
 import fetch from 'node-fetch';
+import fs from 'fs';
 
+const square = await buildSquare({lat: 34.6, long: -107.5}, {lat: 35.4, long: -106.533}, 1000);//half a degree 'square' with 1nm resolution
+fs.writeFileSync('./'+String(square.southWest.lat)+' '+String(square.southWest.long)+'  '+String(square.northEast.lat)+' '+String(square.northEast.long)+'.json', JSON.stringify(square, null, 1));
 
-console.log(feetPerArcSecondAt(0));
-console.log(feetPerArcSecondAt(35));
-const s35latN107long = await buildSquare(35, -107, 6076*2);
-console.log(JSON.stringify(s35latN107long));
-/**
- * 
- * @param {number} latSouthDegree south boundary of square (-90 to 89)
- * @param {number} longWestDegree west boundary of sqaure (-180 to 179)
- * @returns {Object{id: string, multiplier: number, map: []}} returns object with id of the square and an array of arrays that contain elevation of each second (degree, minute, second) for sqaure
- */
-
-function feetPerArcSecondAt(latitude){
-    return Math.cos(Math.abs(latitude)/57.2958) * 101.268591427165;
+function printMap(square){
+    const map = [...square.map];
+    map.reverse();
+    for (let line of map){
+        let lineStr = '';
+        for (let cell of line){
+            lineStr+=String(Math.floor((cell.charCodeAt()*square.multipler+square.altBase)/1000)).padStart(2, ' ');
+        }
+        console.log(lineStr);
+    }
 }
 
-async function buildSquare(latSouthDegree, longWestDegree, desiredMinAccuracy = 1000){//Goes east and north from latDegree, longDegree
-    const latDegree = Math.trunc(latSouthDegree);
-    const longDegree = Math.trunc(longWestDegree);
-    if (latDegree>=89 || longDegree>=179){
-        console.error("latDegree needs to be less than 89 and longDegree needs to be less than 179");
+function isBadLatLong(lat, long){
+    if (lat<=-90 || lat>=90 || long<-180 || long>180) return true;
+    return false;
+}
+
+//TODO manage crossing the -180/180 boundary (e.g. go from 180 long to -179 long)
+
+//default desired accuracy is 303.806 feet (3 arcseconds at equator), at equator best accuracy achieved will be 101.27ft
+async function buildSquare(southWest, northEast, desiredAccuracy = 303.806){
+    if (desiredAccuracy<101.27) desiredAccuracy=101.27;
+
+    if (!southWest || !northEast){
+        console.log('error, southWest and northEast must be an object with lat and long properties')
         return null;
     }
     
-    let divider = 1;
-    let actualMinAccuracyFt;
-    const feetPerArcSecond = Math.round(feetPerArcSecondAt(latDegree <= 0 ? latDegree : latDegree + 1));
-    if (desiredMinAccuracy<=feetPerArcSecond){
-        divider=1;
-        actualMinAccuracyFt=feetPerArcSecond;
-    }else{
-        divider=Math.round(desiredMinAccuracy / feetPerArcSecond);
-        while (3600 % divider){
-            divider++;
-        }
-        actualMinAccuracyFt=divider*feetPerArcSecond;
+    if (isBadLatLong(southWest.lat, southWest.long) || isBadLatLong(northEast.lat, northEast.long)){
+        console.log('error, bad lat long coords');
+        return null;
     }
-    console.log(actualMinAccuracyFt);
-    const square = {id: {lat: latDegree, long: longDegree}, minAccuracy: actualMinAccuracyFt, base: null, multipler: null, map: []};
-
-    const map=[];
-    let lowest=null;
-    let highest=null;
-    let maxProgress=(3600/divider+1)**2;
-    let progess=0;
-    for (let latArcSeconds = 0; latArcSeconds<=3600; latArcSeconds+=divider){
-        const line=[];
-        for (let longArcSeconds = 0; longArcSeconds<=3600; longArcSeconds+=divider){
-            let elevation = null;
-            while (elevation===null){
-                elevation = await fetchElevation(latDegree+latArcSeconds/3600, longDegree+longArcSeconds/3600);
-                if (elevation===null){
-                    console.log('retrying...');
-                }
-            }
-            if (lowest===null || elevation<lowest) lowest=elevation;
-            if (highest===null || elevation>highest) highest=elevation;
-            line.push(elevation);
-            progess++;
-            console.log("progress: ",Math.floor(progess/maxProgress*100)+'%');
-        }
-        map.push(line);
+    if (southWest.lat>northEast.lat){
+        const temp=southWest.lat;
+        southWest.lat=northEast.lat;
+        northEast.lat=temp;
+    }
+    if (southWest.long>northEast.long){
+        const temp=southWest.long;
+        southWest.long=northEast.long;
+        northEast.long=temp;
     }
     
-    square.base=lowest;
-    square.multipler=(highest-lowest)/223;
-    console.log(square.multipler);
-    for (const line of map){
-        let lineStr = '';
-        for (const cell of line){
-            lineStr+=String.fromCharCode(32+Math.floor((cell-lowest)/square.multipler));
-        }
-        square.map.push(lineStr);
-        console.log(lineStr);
+    const latDegreeDiff = Math.abs(southWest.lat-northEast.lat);
+    const longDegreeDiff = Math.abs(southWest.long-northEast.long);
+    
+    const arcSecond = 1/3600;
+    if (latDegreeDiff<arcSecond || longDegreeDiff<arcSecond){
+        console.log('error, latitudes and longitudes must be more than 1 arc second apart');
+        return null;
     }
 
-    return square;
+    //1 arcsecond of latitude = 101.27ft
+    //1 arcsecond of longitude at equator = 101.27ft
+    let widestLatitude = 0;
+    if (southWest.lat>0 && northEast.lat>0){
+        widestLatitude=southWest.lat;
+    } else if (southWest.lat<0 && northEast.lat<0){
+        widestLatitude=northEast.lat;
+    }
 
+
+    const arcSecondsPerStep = desiredAccuracy/101.27;
+    
+    const latArcSeconds = latDegreeDiff*3600;
+    const longArcSeconds = longDegreeDiff*3600;
+
+    const square = {southWest, northEast, altBase: null, multipler: null, accuracy: desiredAccuracy, arcSecondsPerStep: arcSecondsPerStep};
+    const map = [];
+    
+    let lowest=null;
+    let highest=null;
+    let latWriteIndex = 0;
+    let maxProgessTicks = ((latArcSeconds+arcSecondsPerStep)/arcSecondsPerStep)*((longArcSeconds+arcSecondsPerStep)/arcSecondsPerStep);
+    let currentProgress = 0;
+    for (let currentLatBase = 0; (currentLatBase <= latArcSeconds+arcSecondsPerStep); currentLatBase += arcSecondsPerStep){
+        const fetchPromises=[];
+        for (let currentLongBase = 0; (currentLongBase <= longArcSeconds+arcSecondsPerStep); currentLongBase += arcSecondsPerStep){
+            const getLat = southWest.lat + currentLatBase/3600;
+            const getLong = southWest.long + currentLongBase/3600;
+            fetchPromises.push(fetchElevation(getLat, getLong));
+        }
+        let allSettled=false;
+        while (allSettled === false){
+            const settled = await Promise.allSettled(fetchPromises);
+
+            allSettled=true;
+            for (const [index, result] of settled.entries()){
+                if (result.status==='fulfilled'){
+                    currentProgress++;
+                    fetchPromises[index]=result.value;
+                    if (lowest===null || result.value<lowest) lowest=result.value;
+                    if (highest===null || result.value>highest) highest=result.value;
+                }else{
+                    allSettled=false;
+                    fetchPromises[index]=result.reason;
+                }
+            }
+        }
+        console.log('Progress: '+Math.round(currentProgress/maxProgessTicks*100)+'%');
+        map[latWriteIndex++]=[...fetchPromises];
+    }
+    square.altBase=lowest;
+    square.multipler=(highest-lowest)/223;
+    for (const [latIndex, longArray] of map.entries()){
+        for (const [longIndex, elevation] of longArray.entries()){
+            map[latIndex][longIndex] = 32+Math.floor((elevation-lowest)/square.multipler);
+        }
+        map[latIndex] = String.fromCharCode(...map[latIndex]);
+    }
+    square.map=map;
+    
+    return square;
 }
 
 
 
-function fetchElevation(lat, long, count=0){
-    lat=Math.round(lat*100000)/100000;
-    long=Math.round(long*100000)/100000;
-    return fetch('https://nationalmap.gov/epqs/pqs.php?units=feet&output=json&x='+long+'&y='+lat+'callback=JSON_CALLBACK').then( response => {
+function fetchElevation(lat, long, initialDelay=0, count=0){
+    lat=Math.round(lat*1000000)/1000000;
+    long=Math.round(long*1000000)/1000000;
+    const controller = new AbortController();
+    const timeout = setTimeout(() => {
+        controller.abort();
+    }, 2000+Math.min(count*1000, 5000));
+
+    return fetch('https://nationalmap.gov/epqs/pqs.php?units=feet&output=json&x='+long+'&y='+lat+'callback=JSON_CALLBACK', {signal: controller.signal}).then( response => {
         return response.json();
     }).then(result => {
         const q=result.USGS_Elevation_Point_Query_Service.Elevation_Query;
@@ -93,5 +137,7 @@ function fetchElevation(lat, long, count=0){
     }).catch( error => {
         console.log('error fetching #'+count, lat, long, 'retrying...');
         return fetchElevation(lat, long, count+1);
+    }).finally( () => {
+        clearTimeout(timeout);
     });
 }
